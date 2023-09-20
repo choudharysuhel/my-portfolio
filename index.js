5374e726a4a2ca9379968ef703dc90f357b5f8ff
Suhel Choudhary <132272946+choudharysuhel@users.noreply.github.com>
2023-09-20 18:07:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -89,3 +89,13 @@ function checkScroll(){
         initialiseBars();
     }
 }
+
+// popup javascript 
+let popup = document.getElementById("popup");
+
+function openpopup(){
+    popup.classList.add("open-popup");
+}
+function closepopup(){
+    popup.classList.remove("open-popup");
+}
